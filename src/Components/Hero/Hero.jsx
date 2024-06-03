971546ccd2944d0951ecb2de8e6546349b47c715
EmranHossain <emran.shuvo.dev@gmail.com>
2024-06-03 12:01:41 +0600

    added hero section left side contents

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+
+const Hero = () => {
+    return (
+        <>
+        {/* section that will hold everything in this page */}
+            <section>
+                <section>
+                    <h3>Make The Best Financial Decisions</h3>
+                    <p>Cum et convallis risus placerat aliquam, nunc. Scelerisque aliquet faucibus tincidunt eu adipiscing sociis arcu lorem porttitor.</p>
+                    <section>
+                        <button>
+                        </button>
+                        <button>
+                        </button>
+                    </section>
+                    <section>
+
+                    </section>
+                </section>
+                <section>
+
+                </section>
+            </section>
+        </>
+    );
+};
+
+export default Hero;
--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,9 +1,11 @@
+import Hero from "../Components/Hero/Hero";
 import Navbar from "../Components/Navbar/Navbar";
 
 const Main = () => {
     return (
         <>
             <Navbar />  
+            <Hero />
         </>
     );
 };
